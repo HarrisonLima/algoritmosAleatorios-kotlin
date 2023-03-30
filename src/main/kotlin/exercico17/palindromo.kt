6f6c3ed3913ef781338673268147f4a2092943ee
Harrison Fabiano de Lima <harrisonlima92@gmail.com>
2023-03-29 22:12:16 -0300

    Atividade 17 palindromo

---
+++ src/main/kotlin/exercico17/palindromo.kt
@@ -0,0 +1,19 @@
+package exercico17
+
+fun main() {
+    print("Digite uma palavra, frase ou número: ")
+    val string = readLine()!!.trim()
+
+    val isPalindrome = string == string.reversed()
+
+    println("Palavra/Frase/Número: $string")
+    if (isPalindrome) {
+        println("É um palíndromo")
+    } else {
+        println("Não é um palíndromo")
+    }
+}
+
+//➢ Crie um programa que aceite uma palavra, frase ou número.
+//➢ Imprima a palavra e se ela é ou não um palíndromo.
+//➢ Palíndromo é uma palavra, frase ou número que permanece igual quando lida de trás para frente.
