d5bcaea684778eb21f2b165f0af8518320b6a50c
Harrison Fabiano de Lima <harrisonlima92@gmail.com>
2023-03-29 21:24:33 -0300

    Atividades adicionadas

---
+++ src/main/kotlin/exercicio1/twoFer.kt
@@ -0,0 +1,13 @@
+package exercicio1
+fun main() {
+    println("Digite um nome:")
+    var nome = readLine()
+    if (nome == "") {
+        nome = "você";
+    }
+    println(TwoFer(nome))
+}
+
+fun TwoFer(nome: String?): String {
+    return "Um para ${nome}, um para mim."
+}
